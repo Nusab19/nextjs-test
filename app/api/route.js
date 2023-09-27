02b0ac92c38ff183c00038bdcf72257408cc7030
Nusab Taha <nusabtaha33@gmail.com>
2023-09-27 11:23:56 +0600

    another test

--- app/api/route.js
+++ app/api/route.js
@@ -1,12 +1,11 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
   const data = {
     ok: true,
     ip: req.query?.clientIp ?? "127.0.0.1",
+    ip2: req.headers.get("x-real-ip") ?? "not-found",
     message: "Hello from the API",
   };
   return new NextResponse(JSON.stringify(data, null, 2));
 }
-
-
