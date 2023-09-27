4a29551904695d3aa1c49b5831c55716acd976a1
Nusab Taha <nusabtaha33@gmail.com>
2023-09-27 12:50:04 +0600

    new method

---
+++ app/api/ip/route.js
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { headers } from "next/headers";
+
+export async function GET(req) {
+  const { ua } = userAgent(req);
+
+  const headersList = headers();
+  const ip = headersList.get("x-forwarded-for");
+
+  const data = {
+    ok: true,
+    ip_address: ip ?? "127.0.0.1",
+    user_agent: ua,
+  };
+
+  return NextResponse.json(data, { status: 200 });
+}
