235731864d8c1b09b911254556f2d5f6627b6b28
Nusab Taha <nusabtaha33@gmail.com>
2023-09-27 10:30:35 +0600

    middleware at root

--- app/api/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function middleware(req) {
-  const { ip, nextUrl } = req;
-
-  nextUrl.searchParams.set('clientIp', ip);
-
-  return NextResponse.rewrite(nextUrl);
-}
